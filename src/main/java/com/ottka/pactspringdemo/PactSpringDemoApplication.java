475bc38ac26adc6bfd1a6d604fdbc6a839ba30cb
Ossi Ottka <ossiottka@microsoft.com>
2020-12-07 11:37:13 +0100

    Added spring boot skeleton

---
+++ src/main/java/com/ottka/pactspringdemo/PactSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ottka.pactspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactSpringDemoApplication.class, args);
+	}
+
+}
